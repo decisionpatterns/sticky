0971b49e9038f70bb6d53f93be930ee5e187daf0
Christopher Brown <cbrown@decisionpatterns.com>
2016-09-25 21:49:27 -0700

    Add zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,17 @@
+.onAttach <- function( libname, pkgname ) {
+
+  suppressWarnings( try( v <- utils::packageVersion(pkgname, libname), silent = TRUE ))
+  version <- if( exists('v') ) paste0("-", v ) else ""
+
+  if( interactive() )
+    packageStartupMessage(
+      pkgname ,
+      version ,
+      " - Copyright \u00a9 ", substr(Sys.Date(),1,4),
+      " Decision Patterns" ,
+      domain = NA
+    )
+
+
+}
+
