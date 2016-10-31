9230e15f3df3147c7700772de0f0850b11cf92da
Christopher Brown <cbrown@decisionpatterns.com>
2016-10-31 15:29:42 -0700

    Fix for #2
     - #2: now matrices are properly supported.
       - Accomodate `mostattributes` for matrices to propogate to result of `extract1`.
       - Adds tests for matrices.
     - add utils.R
     - add issues/

--- R/extract1.R
+++ R/extract1.R
@@ -5,14 +5,27 @@
 #' @param ... additional parameters
 #'
 #' @details
-#' -tk
+#'
+#' Implements `[` for sticky attributes. This methods ensures that existing
+#' attributes are propogated forward.
+#'
+#' @seealso
+#'   \code{\link[base]{attributes}}, specifically \code{mostattributes}
 #'
 #' @rdname extract
 #' @include sticky.R
 #' @export
 
 `[.sticky` <- function(x,...) {
+
   r <- NextMethod("[")
+
+  has_dim <- has_attr(r, 'dim')
+  r_dim <- dim(r)
+
   mostattributes(r) <- attributes(x)
+
+  if( has_dim ) dim(r) <- r_dim
+
   r
 }
